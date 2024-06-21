f13d56e3c022f68c264278bb424ac70ea6a8373f
Tanya Davis <swe.la@outlook.com>
2024-06-21 07:23:49 -0400

    Added slideshow feature

--- scripts.js
+++ scripts.js
@@ -1,12 +1,33 @@
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].classList.add('active');
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
 
-
-
-
-
-
-
-
+function plusSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    slideIndex += n;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    slides[slideIndex-1].classList.add('active');
+}
 
 
 document.getElementById('takeoff-button').addEventListener('click', function(event) {
